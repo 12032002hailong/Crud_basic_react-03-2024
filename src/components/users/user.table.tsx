1e23858ab1b86bff3d647f744229f2d816ada4ac
HLong <hailongkc1203@gmail.com>
2024-02-24 20:54:30 +0700

    add new user

--- src/components/users/user.table.tsx
+++ src/components/users/user.table.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Button, Modal, Table, Tag } from "antd";
+import { Button, Modal, Table, Tag, notification } from "antd";
 import type { TableProps } from "antd";
 import type { ColumnsType } from "antd/es/table";
 import { Header } from "antd/es/layout/layout";
@@ -25,18 +25,34 @@ const UsersTable = () => {
   const [address, setAddress] = useState("");
   const [role, setRole] = useState("");
 
-  const showModal = () => {
-    setIsModalOpen(true);
-  };
+  const access_token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0b2tlbiBsb2dpbiIsImlzcyI6ImZyb20gc2VydmVyIiwiX2lkIjoiNjVkNGEzMTJmODU3ZjcwY2U0MGFiNjc4IiwiZW1haWwiOiJhZG1pbkBnbWFpbC5jb20iLCJhZGRyZXNzIjoiVmlldE5hbSIsImlzVmVyaWZ5Ijp0cnVlLCJuYW1lIjoiSSdtIGFkbWluIiwidHlwZSI6IlNZU1RFTSIsInJvbGUiOiJBRE1JTiIsImdlbmRlciI6Ik1BTEUiLCJhZ2UiOjY5LCJpYXQiOjE3MDg2ODgxODQsImV4cCI6MTc5NTA4ODE4NH0.2ZaF98bZxvA4gDt2aMCbXjhytfZdOh-OFAql9nVovQ0";
 
-  const handleOk = () => {
+  const handleOk = async () => {
     setIsModalOpen(false);
     const data = { name, email, password, age, gender, address, role };
-    console.log("data", data);
-  };
+    const res = await fetch("http://localhost:8000/api/v1/users", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ ...data }),
+    });
 
-  const handleCancel = () => {
-    setIsModalOpen(false);
+    const d = await res.json();
+    if (d.data) {
+      await getData();
+      notification.success({
+        message: "Tao moi nguoi dung thanh cong",
+      });
+      setIsModalOpen(false);
+    } else {
+      notification.error({
+        message: "Co loi xay ra",
+        description: JSON.stringify(d.message),
+      });
+    }
   };
 
   useEffect(() => {
@@ -44,9 +60,6 @@ const UsersTable = () => {
   }, []);
 
   const getData = async () => {
-    const access_token =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0b2tlbiBsb2dpbiIsImlzcyI6ImZyb20gc2VydmVyIiwiX2lkIjoiNjVkNGEzMTJmODU3ZjcwY2U0MGFiNjc4IiwiZW1haWwiOiJhZG1pbkBnbWFpbC5jb20iLCJhZGRyZXNzIjoiVmlldE5hbSIsImlzVmVyaWZ5Ijp0cnVlLCJuYW1lIjoiSSdtIGFkbWluIiwidHlwZSI6IlNZU1RFTSIsInJvbGUiOiJBRE1JTiIsImdlbmRlciI6Ik1BTEUiLCJhZ2UiOjY5LCJpYXQiOjE3MDg2ODgxODQsImV4cCI6MTc5NTA4ODE4NH0.2ZaF98bZxvA4gDt2aMCbXjhytfZdOh-OFAql9nVovQ0";
-
     const res = await fetch("http://localhost:8000/api/v1/users/all", {
       headers: {
         Authorization: `Bearer ${access_token}`,
@@ -56,6 +69,21 @@ const UsersTable = () => {
 
     const data1 = await res.json();
     setListUsers(data1.data.result);
+  };
+
+  const clearData = () => {
+    setName("");
+    setEmail("");
+    setPassword("");
+    setAge("");
+    setGender("");
+    setAddress("");
+    setRole("");
+  };
+
+  const handleCloseCreateModal = () => {
+    setIsModalOpen(false);
+    clearData();
   };
 
   const columns: ColumnsType<IUsers> = [
@@ -83,7 +111,11 @@ const UsersTable = () => {
         }}
       >
         <h2 style={{ color: "#c94087" }}>Table</h2>
-        <Button type="primary" icon={<PlusOutlined />} onClick={showModal}>
+        <Button
+          type="primary"
+          icon={<PlusOutlined />}
+          onClick={() => setIsModalOpen(true)}
+        >
           Add new user
         </Button>
       </Header>
@@ -94,7 +126,7 @@ const UsersTable = () => {
         title="Basic Modal"
         open={isModalOpen}
         onOk={handleOk}
-        onCancel={handleCancel}
+        onCancel={() => handleCloseCreateModal()}
         maskClosable={false}
       >
         <div>
